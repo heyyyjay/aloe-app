eb3d8afb2f530d767801a2333e57d9c025025242
J Tolentino <jltolent@gmail.com>
2014-08-25 20:31:18 -0700

    refactor: removed switch statements

--- js/Task.js
+++ js/Task.js
@@ -14,51 +14,18 @@ Task.prototype.createHtml = function() {
 		class: "task uk-width-1-1"
 	});
 
+	html.addClass("priority-" + this.priority);
+	html.addClass("category-" + this.category);
+
 	var check = $("<form>", {
 		class: "uk-form check"
 	});
-
 	var box = $("<input>", {
 		id: this.taskId + "checkbox",
 		type: "checkbox",
 		"onclick": "checkOff(" + this.taskId + "Descr)"
 	});
-	console.log(this.taskId);
 	check.append(box);
-
-	html.append(check);
-
-	switch(this.priority) {
-		case "high":
-			html.addClass("priority-high");
-			break;
-		case "mid":
-			html.addClass("priority-mid");
-			break;
-		default:
-			html.addClass("priority-low");
-			break;
-	}
-	switch(this.category) {
-		case "0":
-			html.addClass("category-0");
-			break;
-		case "1":
-			html.addClass("category-1");
-			break;
-		case "2":
-			html.addClass("category-2");
-			break;
-		case "3":
-			html.addClass("category-3");
-			break;
-		case "4":
-			html.addClass("category-4");
-			break;
-		default:
-			html.addClass("category-5");
-			break;
-	}
 
 	html.append($("<div>", {
 		id: this.taskId + "Descr",
